c7b343f659855a3a785267ff3c0e917f075dbb66
Nicolai Stein <nicolai_stein@outlook.de>
2023-12-07 19:45:31 +0100

    get user from session

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+import requests
+from jose import jwt
 from dotenv import load_dotenv
 import os
 
@@ -13,20 +15,63 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 PROJECT_ID = os.getenv("PROJECT_ID", "pro-xxx")
+API_SECRET = os.getenv("API_SECRET", "corbado1_xxx")
 
-@app.get("/login", response_class=HTMLResponse)
+# Session config
+short_session_cookie_name = "cbo_short_session"
+issuer = f"https://{PROJECT_ID}.frontendapi.corbado.io"
+jwks_uri = f"https://{PROJECT_ID}.frontendapi.corbado.io/.well-known/jwks"
+
+
+
+@app.get("/", response_class=HTMLResponse)
 async def get_login(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "PROJECT_ID": PROJECT_ID})
 
 
 @app.get("/profile", response_class=HTMLResponse)
 async def get_profile(request: Request):
-    return templates.TemplateResponse("profile.html", {"request": request, "PROJECT_ID": PROJECT_ID})
+    token = request.cookies.get(short_session_cookie_name)
 
+    try:
+        if not token:
+            raise ValueError("No token found")
+
+        response = requests.get(jwks_uri)
+        response.raise_for_status()
+
+        jwks = response.json()
+        public_key = jwks["keys"][0]
+
+        payload = jwt.decode(
+            token,
+            key=public_key,
+            algorithms=["RS256"],
+            audience=API_SECRET,
+            issuer=issuer,
+        )
+
+        if payload["iss"] != issuer:
+            raise ValueError("Invalid issuer")
+
+        context = {
+            'request': request,
+            'PROJECT_ID': PROJECT_ID,
+            'USER_ID': payload["sub"],
+            'USER_NAME': payload.get("name"),
+            'USER_EMAIL': payload.get("email"),
+         }
+
+        return templates.TemplateResponse("profile.html", context)
+    
+    except Exception as e:
+        print(e)
+        return RedirectResponse(url="/login")
+    
 
 @app.get("/{path}", response_class=HTMLResponse)
 async def redirect_to_login(path: str):
-    return RedirectResponse(url="/login")
+    return RedirectResponse(url="/")
 
 
 if __name__ == "__main__":
