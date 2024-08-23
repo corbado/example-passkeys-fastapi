e83e6308f059033a62eb95a9027fc2645b9172b7
Aleksandr Balakirev <santey3@gmail.com>
2024-08-23 18:15:49 +0200

    update for never passkeys version, update requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from typing import List
+from corbado_python_sdk.entities.session_validation_result import (
+    SessionValidationResult,
+)
+from corbado_python_sdk.generated.models.identifier import Identifier
+
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 import os
-from corbado_python_sdk import Config, CorbadoSDK, UserEntity, SessionInterface
+from corbado_python_sdk import (
+    Config,
+    CorbadoSDK,
+    IdentifierInterface,
+    SessionInterface,
+)
+
 
 load_dotenv()
 
@@ -27,6 +39,7 @@ config: Config = Config(
 # Initialize SDK
 sdk: CorbadoSDK = CorbadoSDK(config=config)
 sessions: SessionInterface = sdk.sessions
+identifiers: IdentifierInterface = sdk.identifiers
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -40,20 +53,30 @@ async def get_login(request: Request):
 async def get_profile(request: Request):
     # Acquire cookies with your preferred method
     token: str = request.cookies.get(config.short_session_cookie_name) or ""
-    user: UserEntity = sessions.get_current_user(short_session=token)
+    validation_result: SessionValidationResult = (
+        sessions.get_and_validate_short_session_value(short_session=token)
+    )
+    if validation_result.authenticated:
 
-    if user.authenticated:
+        emailList: List[Identifier] = identifiers.list_all_emails_by_user_id(
+            user_id=validation_result.user_id
+            or ""  # at this point user_id should be non empty string since user was authenticated
+        )
+
         context = {
             "request": request,
             "PROJECT_ID": PROJECT_ID,
-            "USER_ID": user.user_id,
-            "USER_NAME": user.name,
-            "USER_EMAIL": user.email,
+            "USER_ID": validation_result.user_id,
+            "USER_NAME": validation_result.full_name,
+            "USER_EMAIL": emailList[0].value,
         }
         return templates.TemplateResponse("profile.html", context)
 
     else:
-        return Response(content="You are not authenticated or have not yet confirmed your email.", status_code=401)
+        return Response(
+            content="You are not authenticated or have not yet confirmed your email.",
+            status_code=401,
+        )
 
 
 if __name__ == "__main__":
